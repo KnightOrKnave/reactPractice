1f522fa265f7c0f968f9fe58a9b7733903b684e3
kqhome <a11dc094@dhw.ac.jp>
2019-11-13 23:05:14 +0900

    f

--- saga-test-2/weather-view/src/sagas/index.js
+++ saga-test-2/weather-view/src/sagas/index.js
@@ -1,9 +1,9 @@
 import { all, call, put, takeEvery, delay } from "redux-saga/effects";
-const request = require("request");
 
 export function* test(action) {
   console.log("saga called");
   const res = yield call(getRequest);
+  console.log(res);
   if (res) {
     console.log(res["colors"][0].value);
     yield put({ type: "TEST2", id: res["colors"][0].value });
@@ -12,18 +12,26 @@ export function* test(action) {
   }
 }
 
+function getRequest() {
+  console.log("getRequest Called")
+  return fetch('https://api.noopschallenge.com/hexbot',{
+    method:'get',
+  })
+  .then((res)=>{
+    console.log("retch return start")
+    console.log("*"+res+"*");
+    var rr=res.json()
+    var rrr = Promise.resolve(rr);
+    console.log("**"+rrr+"**");
+    return (Promise.resolve(rr));
+  })
+  .catch(error=>({error}));
+}
+
 //最初に走る
 export function* mySaga() {
   console.log("mysaga called");
   yield takeEvery("TEST", test);
-}
-
-function getRequest() {
-  return fetch('https://api.noopschallenge.com/hexbot',{
-    method:'get',
-  })
-  .then(res=>(res.json()))
-  .catch(error=>({error}));
 }
 
 export default function* rootSaga() {
